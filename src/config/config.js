a45f43ca3c09ee06f1366bdf3498e2874a37af51
Shivam Rajput <shivam.rajput@thoughtworks.com>
2022-02-24 13:19:49 +0530

    updated config

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,5 @@
 // eslint-disable-next-line no-process-env
-export const BFF_URL = process.env.BFF_URL || 'http://localhost:3001'
+export const BFF_URL = process.env.NODE_ENV === 'production'
+  ? 'https://shivi-board-games-bff.herokuapp.com'
+  : 'http://localhost:3001'
 
