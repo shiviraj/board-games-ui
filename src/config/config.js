83d7ba516311364fc855f6e19433a9c75d1adb77
Shivam Rajput <shivam.rajput@thoughtworks.com>
2022-02-24 13:13:09 +0530

    updated env variable

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,3 @@
 // eslint-disable-next-line no-process-env
-export const BFF_URL = process.env.NODE_ENV === 'production'
-  ? 'https://shivipoetry-bff.herokuapp.com'
-  : 'http://localhost:3001'
+export const BFF_URL = process.env.BFF_URL || 'http://localhost:3001'
 
